6e477a42783121ad2791e17914ab5c304fe065d8
fideloper <fideloper@gmail.com>
2013-12-28 20:05:16 -0500

    Fixing conditionals that were testing nginx and apache checks

--- laravel.sh
+++ laravel.sh
@@ -19,12 +19,12 @@ NGINX_IS_INSTALLED=$?
 apache2 -v 2>&1 >/dev/null
 APACHE_IS_INSTALLED=$?
 
-if [ NGINX_IS_INSTALLED -eq 0 ]; then
+if [ $NGINX_IS_INSTALLED -eq 0 ]; then
     # Change default vhost created
     sed -i "s/root \/vagrant/root \/vagrant\/laravel\/public" /etc/nginx/sites-available/vagrant
 fi
 
-if [ APACHE_IS_INSTALLED -eq 0 ]; then
+if [ $APACHE_IS_INSTALLED -eq 0 ]; then
     # Remove apache vhost from default and create a new one
     rm /etc/apache2/sites-enabled/192.168.33.10.xip.io.conf 2>&1 >/dev/null
     rm /etc/apache2/sites-available/192.168.33.10.xip.io.conf 2>&1 >/dev/null
