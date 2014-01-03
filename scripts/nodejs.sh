a851c5394a3f502a3501d8fd756ad6f3ec4f06c5
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-01-03 12:14:23 -0200

    Install Node.js from http://nodejs.org/dist/

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 
-echo ">>> Adding PPA's and Installing Node.js"
+# Set the install directory (/usr/local is fine for Ubuntu or Debian)
+INSTALL_DIR=/usr/local
 
-# Add repo for latest Node.js
-sudo add-apt-repository -y ppa:chris-lea/node.js
+# Which version of Node.js do you wish to install?
+NODEJS_VERSION=latest
+## you can define an older version: NODEJS_VERSION=v0.10.22
 
-# Update
-sudo apt-get update
+# If set to latest, get the current node version from the home page
+if [ "$NODEJS_VERSION" == "latest" ]; then
+    NODEJS_VERSION=`curl 'nodejs.org' | grep 'Current Version' | awk '{ print $3 }' | awk -F\< '{ print $1 }'`
+fi
 
-# Install node.js
-sudo apt-get install -y nodejs
+echo ">>> Installing Node.js $NODEJS_VERSION"
+
+# Current system architecture
+PLATFORM=linux
+ARCH=x`getconf LONG_BIT`
+
+# Check if the version to be installed isn't already installed
+if [[ "`type -p npm`" != "" ]]; then
+    if [[ "`node --version`" == "$NODEJS_VERSION" ]]; then
+        echo "The correct node version ($NODEJS_VERSION) is already installed"
+        exit 1
+    fi
+fi
+
+# Install Node.js - download binaries and uncompress them in the install dir
+sudo curl http://nodejs.org/dist/$NODEJS_VERSION/node-$NODEJS_VERSION-$PLATFORM-$ARCH.tar.gz \
+        | sudo tar xzvf - --strip-components=1 -C "$INSTALL_DIR"
 
 # Change where npm global packages location
 npm config set prefix ~/npm
