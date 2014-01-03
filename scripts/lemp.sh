86055173484462ecd51312090a6b5e1dec2dc7b4
fideloper <fideloper@gmail.com>
2014-01-02 19:43:56 -0500

    removed git from nginx install

--- scripts/lemp.sh
+++ scripts/lemp.sh
@@ -12,7 +12,7 @@ sudo add-apt-repository -y ppa:nginx/stable
 sudo apt-get update
 
 # Install the Rest
-sudo apt-get install -y git-core nginx php5-fpm php5-cli php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-mcrypt php5-xdebug
+sudo apt-get install -y nginx php5-fpm php5-cli php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-mcrypt php5-xdebug
 
 echo ">>> Configuring Server"
 
@@ -75,7 +75,3 @@ sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
 
 sudo service php5-fpm restart
 sudo service nginx restart
-
-# Git Config and set Owner
-curl https://gist.github.com/fideloper/3751524/raw/.gitconfig > /home/vagrant/.gitconfig
-sudo chown vagrant:vagrant /home/vagrant/.gitconfig
