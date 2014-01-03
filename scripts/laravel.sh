b765ab53d3b8cdb003771a6164f2c34871ab508c
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-01-03 13:13:23 -0200

    Install Laravel via Laravel PHAR installer

--- scripts/laravel.sh
+++ scripts/laravel.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
+LARAVEL_APP=/usr/local/bin/laravel
+
 echo ">>> Installing Laravel"
 
-# Test if Composer is installed
-composer --version > /dev/null 2>&1
-COMPOSER_IS_INSTALLED=$?
+# Check if all needed PHP 5 components are installed
+PHP5_INSTALLED=`apt-cache policy php5 | grep Installed | grep -v "(none)"`
+PHP5_CURL_INSTALLED=`apt-cache policy php5-curl | grep Installed | grep -v "(none)"`
+PHP5_MCRYPT_INSTALLED=`apt-cache policy php5-mcrypt | grep Installed | grep -v "(none)"`
 
-if [ $COMPOSER_IS_INSTALLED -gt 0 ]; then
-    echo "ERROR: Laravel install requires composer"
+if [[ "$PHP5_INSTALLED" == "" || "$PHP5_CURL_INSTALLED" == "" || "$PHP5_MCRYPT_INSTALLED" == "" ]]; then
+    echo "ERROR: Laravel requires the php5, php5-curl and php5-mcrypt packages"
     exit 1
 fi
 
-# Create Laravel
-composer create-project --prefer-dist laravel/laravel /vagrant/laravel
+# Install Laravel PHAR
+sudo wget --no-check-certificate -O $LARAVEL_APP http://laravel.com/laravel.phar
+sudo chmod +x $LARAVEL_APP
+
+# Create laravel application
+cd /vagrant
+$LARAVEL_APP new laravel
 
 # Set new document root on Apache or Nginx
 nginx -v > /dev/null 2>&1
