7fa71849a085120498f21770329a6d35e5416b66
fideloper <fideloper@gmail.com>
2014-01-03 21:09:24 -0500

    zsh updated to run as vagrant

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -6,7 +6,7 @@ echo ">>> Installing Oh-My-Zsh"
 sudo apt-get install -y zsh
 
 # Install oh-my-zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+sudo su - vagrant -c 'wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh'
 
 # Change vagrant user's default shell
 sudo su - vagrant -c 'chsh -s `which zsh`'
