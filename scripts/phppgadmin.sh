f17c3a8ade300511e53dac8d7124f6261ab131cc
Marco de Vries <mrdevries87@gmail.com>
2014-01-06 20:40:30 +0100

    Update phppgadmin.sh
    Only install with Apache2, fixed logging in with root

--- scripts/phppgadmin.sh
+++ scripts/phppgadmin.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-echo ">>> Installing phpPgAdmin"
+apache2 -v > /dev/null 2>&1
+APACHE_IS_INSTALLED=$?
 
-# Install phpPgAdmin
-sudo apt-get install -y phppgadmin
+if [ $APACHE_IS_INSTALLED -eq 0 ]; then
+    echo ">>> Installing phpPgAdmin"
 
-# Set and enable configuration
-sudo mv /etc/apache2/conf.d/phppgadmin /etc/apache2/conf-available/phppgadmin.conf
-sudo a2enconf phppgadmin
+    # Install phpPgAdmin depending on Apache
+    sudo apt-get install -y phppgadmin
 
-# Reload Apache to load in configuration
-sudo service apache2 reload
+    # Allow logging in with user 'root'
+    sudo sed -i "s/$conf\['extra_login_security'\] = true;/$conf\['extra_login_security'\] = false;/" /usr/share/phppgadmin/conf/config.inc.php
+
+    # Set and enable configuration for Apache
+    sudo mv /etc/apache2/conf.d/phppgadmin /etc/apache2/conf-available/phppgadmin.conf
+    sudo sed -i "s/allow from 127.0.0.0\/255.0.0.0 ::1\/128/# allow from 127.0.0.0\/255.0.0.0 ::1\/128/" /etc/apache2/conf-available/phppgadmin.conf
+    sudo sed -i "s/# allow from all/allow from all/" /etc/apache2/conf-available/phppgadmin.conf
+    sudo a2enconf phppgadmin
+
+    # Reload Apache to load in configuration
+    sudo service apache2 reload
+fi
