4ed7803675f2e20458b3c5b6ba57850eadad42f0
Marco de Vries <mrdevries87@gmail.com>
2014-01-07 23:49:34 +0100

    Create elasticsearch.sh
    vagrant@precise64:~$ curl http://localhost:9200
    {
      "ok" : true,
      "status" : 200,
      "name" : "Robertson, Robbie",
      "version" : {
        "number" : "0.90.9",
        "build_hash" : "a968646da4b6a2d9d8bca9e51e92597fe64e8d1a",
        "build_timestamp" : "2013-12-23T10:35:28Z",
        "build_snapshot" : false,
        "lucene_version" : "4.6"
      },
      "tagline" : "You Know, for Search"
    }
    
    Yay search :)

---
+++ scripts/elasticsearch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo ">>> Installing Elasticsearch"
+
+# Set some variables
+ELASTICSEARCH_VERSION=0.90.9 # Check http://www.elasticsearch.org/download/ for latest version
+
+# Install prerequisite: Java
+sudo apt-get install -y openjdk-7-jre-headless
+
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.deb
+sudo dpkg -i elasticsearch-$ELASTICSEARCH_VERSION.deb
+rm elasticsearch-$ELASTICSEARCH_VERSION.deb
+
+# Configure Elasticsearch for development purposes (1 shard/no replicas, don't allow it to swap at all if it can run without swapping)
+sudo sed -i "s/# index.number_of_shards: 1/index.number_of_shards: 1/" /etc/elasticsearch/elasticsearch.yml
+sudo sed -i "s/# index.number_of_replicas: 0/index.number_of_replicas: 0/" /etc/elasticsearch/elasticsearch.yml
+sudo sed -i "s/# bootstrap.mlockall: true/bootstrap.mlockall: true/" /etc/elasticsearch/elasticsearch.yml
+sudo service elasticsearch restart
