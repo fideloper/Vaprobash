3f542abf2f01917801dff25f65ca35a4e57a4ada
Patrik Storm <storm.patrik@gmail.com>
2014-01-10 15:17:21 +0200

    added bmp support for faster calculations.

--- scripts/php.sh
+++ scripts/php.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository -y ppa:ondrej/php5
 sudo apt-get update
 
 # Install PHP
-sudo apt-get install -y php5-cli php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-mcrypt php5-xdebug php5-memcached
+sudo apt-get install -y php5-cli php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-gmp php5-mcrypt php5-xdebug php5-memcached
 
 # xdebug Config
 cat > /etc/php5/mods-available/xdebug.ini << EOF
