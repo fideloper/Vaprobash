caa94f08f1f1c4401ed47f2a488396fa3585520d
fideloper <fideloper@gmail.com>
2014-02-17 21:25:00 -0500

    nginx install updated for ssl

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -20,6 +20,51 @@ echo ">>> Configuring Nginx"
 # is not escaped
 cat > /etc/nginx/sites-available/vagrant << EOF
 server {
+    listen  80;
+
+    root /vagrant;
+    index index.html index.htm index.php;
+
+    # Make site accessible from http://set-ip-address.xip.io
+    server_name $1.xip.io;
+
+    access_log /var/log/nginx/vagrant.com-access.log;
+    error_log  /var/log/nginx/vagrant.com-error.log error;
+
+    charset utf-8;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location = /favicon.ico { log_not_found off; access_log off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    error_page 404 /index.php;
+
+    # pass the PHP scripts to php5-fpm
+    location ~ \.php$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        # With php5-fpm:
+        fastcgi_pass unix:/var/run/php5-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_param LARA_ENV local; # Environment variable for Laravel
+        include fastcgi_params;
+    }
+
+    # Deny .htaccess file access
+    location ~ /\.ht {
+        deny all;
+    }
+}
+
+server {
+    listen 443;
+
+    ssl on;
+    ssl_certificate     /etc/ssl/xip.io/xip.io.crt;
+    ssl_certificate_key /etc/ssl/xip.io/xip.io.key;
+
     root /vagrant;
     index index.html index.htm index.php;
 
@@ -66,7 +111,9 @@ curl https://gist.github.com/fideloper/8261546/raw/ngxdis > ngxdis
 sudo chmod guo+x ngxen ngxdis
 sudo mv ngxen ngxdis /usr/local/bin
 
-# setup the vhost generator script for nginx
+# Setup the vhost generator script for nginx
+# This sould be used for the above setup eventually, rather
+# than the hard-coded config above!
 curl https://gist.github.com/fideloper/9063376/raw > ngxvhost
 sudo chown root:root ngxvhost
 sudo chmod guo+x ngxvhost
