e51336e8166f82cef8bb63981cc52e750e3d5fdf
Brian Retterer <bretterer@gmail.com>
2014-02-20 18:47:38 -0500

    Add mssql.sh for letting laravel connect to SQL Server

--- scripts/php.sh
+++ scripts/php.sh
@@ -20,7 +20,7 @@ fi
 sudo apt-get update
 
 # Install PHP
-sudo apt-get install -y php5-cli php5-fpm php5-mysql php5-mssql php5-pgsql php5-sqlite php5-curl php5-gd php5-gmp php5-mcrypt php5-xdebug php5-memcached php5-imagick
+sudo apt-get install -y php5-cli php5-fpm php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-gmp php5-mcrypt php5-xdebug php5-memcached php5-imagick
 
 # xdebug Config
 cat > /etc/php5/mods-available/xdebug.ini << EOF
