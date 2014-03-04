c7b89c2879088d9872527a1ce3de41b11a031bfc
ke20 <k_auvinet@hotmail.fr>
2014-03-04 17:08:27 +0100

    add params for var_dump display

--- scripts/php.sh
+++ scripts/php.sh
@@ -33,6 +33,11 @@ xdebug.remote_port = 9000
 xdebug.scream=1
 xdebug.cli_color=1
 xdebug.show_local_vars=1
+
+; var_dump display
+xdebug.var_display_max_depth = 5
+xdebug.var_display_max_children = 256
+xdebug.var_display_max_data = 1024 
 EOF
 
 # PHP Error Reporting Config
