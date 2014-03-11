f5b0b8254efbdfaa1dd6a26bd315b4ed60e17466
Daniel Hollands <daniel@limeblast.co.uk>
2014-03-11 12:47:31 +0000

    Update hhvm.sh

--- scripts/hhvm.sh
+++ scripts/hhvm.sh
@@ -3,7 +3,7 @@
 echo ">>> Installing HHVM"
 
 # Get key and add to sources
-sudo add-apt-repository ppa:mapnik/boost
+sudo add-apt-repository -y ppa:mapnik/boost
 wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
 echo deb http://dl.hhvm.com/ubuntu precise main | sudo tee /etc/apt/sources.list.d/hhvm.list
 
