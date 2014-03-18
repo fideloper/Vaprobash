0e40b5bc29c23278480b733ab722fdf094bf01b2
Kris Kuchenbaur <kris@myndful.net>
2014-03-18 15:52:39 -0400

    repo clean up

--- scripts/mailcatcher.sh
+++ scripts/mailcatcher.sh
@@ -5,40 +5,38 @@ echo ">>> Installing Mailcatcher"
 #dependency
 sudo apt-get install -y libsqlite3-dev
 
+# Check if RVM is installed
+rvm -v > /dev/null 2>&1
+RVM_IS_INSTALLED=$?
 
-if $(which rvm) -v > /dev/null 2>&1; then
+if [[ $RVM_IS_INSTALLED -eq 0 ]]; then
 	echo ">>>>Installing with RVM"
-	$(which rvm) default@mailcatcher --create do gem install --no-rdoc --no-ri mailcatcher
-	$(which rvm) wrapper default@mailcatcher --no-prefix mailcatcher catchmail
+	rvm default@mailcatcher --create do gem install mailcatcher --no-rdoc --no-ri
+	rvm wrapper default@mailcatcher --no-prefix mailcatcher catchmail
 else
 	#gem check
-	if ! gem -v > /dev/null 2>&1; then sudo aptitude install -y libgemplugin-ruby; fi
+	sudo aptitude install -y libgemplugin-ruby;
 
 	#install
-	gem install --no-rdoc --no-ri mailcatcher
+	sudo su vagrant -c 'sudo gem install mailcatcher --no-ri --no-rdoc'
 fi
-
-
-#make it start on boot
-sudo echo "@reboot $(which mailcatcher) --ip=0.0.0.0" >> /etc/crontab
-sudo update-rc.d cron defaults
 
 #make php use it to send mail
 sudo echo "sendmail_path = /usr/bin/env $(which catchmail)" >> /etc/php5/mods-available/mailcatcher.ini
 sudo php5enmod mailcatcher
 sudo service php5-fpm restart
-sudo service apache2 restart
-
-#start it now
-/usr/bin/env $(which mailcatcher) --ip=0.0.0.0
 
 #add aliases
 if [[ -f "/home/vagrant/.profile" ]]; then
-	sudo echo "alias mailcatcher=\"mailcatcher --ip=0.0.0.0\"" >> /home/vagrant/.profile
+	echo -e "\nalias mailcatcher=\"mailcatcher --ip=0.0.0.0\"" >> /home/vagrant/.profile
 	. /home/vagrant/.profile
 fi
 
 if [[ -f "/home/vagrant/.zshrc" ]]; then
-	sudo echo "alias mailcatcher=\"mailcatcher --ip=0.0.0.0\"" >> /home/vagrant/.zshrc
+	echo -e "\nalias mailcatcher=\"mailcatcher --ip=0.0.0.0\"" >> /home/vagrant/.zshrc
 	. /home/vagrant/.zshrc
 fi
+
+#start it
+sudo su vagrant -c "mailcatcher --ip=0.0.0.0"
+
