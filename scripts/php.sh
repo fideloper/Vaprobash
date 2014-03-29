53e49cd55ea711c57a556e5d8a19b764113d0e86
fideloper <fideloper@gmail.com>
2014-03-28 22:30:43 -0400

    updated php

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]; then
-    php_version="distributed"
-else
-    php_version="$1"
-fi
+echo ">>> Installing PHP"
 
-echo ">>> Installing PHP $1 version"
-
-if [ $php_version == "latest" ]; then
-    sudo add-apt-repository -y ppa:ondrej/php5
-fi
-
-if [ $php_version == "previous" ]; then
-    sudo add-apt-repository -y ppa:ondrej/php5-oldstable
-fi
+sudo add-apt-repository -y ppa:ondrej/php5
 
 sudo apt-get update
 
@@ -40,15 +28,9 @@ EOF
 # PHP Error Reporting Config
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
-sed -i "s/html_errors = .*/html_errors = On/" /etc/php5/fpm/php.ini
 
 # PHP Date Timezone
 sed -i "s/;date.timezone =.*/date.timezone = ${2/\//\\/}/" /etc/php5/fpm/php.ini
 sed -i "s/;date.timezone =.*/date.timezone = ${2/\//\\/}/" /etc/php5/cli/php.ini
-
-# Make sure php5-fpm is running as a Unix socket on "distributed" version
-if [ $php_version == "distributed" ]; then
-    sed -i "s/listen = .*/listen = \/var\/run\/php5-fpm.sock/" /etc/php5/fpm/pool.d/www.conf
-fi
 
 sudo service php5-fpm restart
