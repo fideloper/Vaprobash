18676651ccd6af5e32d394eae757a6e71d4102fd
fideloper <fideloper@gmail.com>
2014-03-31 19:48:23 -0400

    updated hhvm with flags for fastCGI and overtaking php

--- scripts/hhvm.sh
+++ scripts/hhvm.sh
@@ -12,3 +12,16 @@ sudo apt-get update
 
 # Install HHVM
 sudo apt-get install -y hhvm
+
+# Use as FastCGI?
+if [ "$1" == "true" ]; then
+    sudo /usr/share/hhvm/install_fastcgi.sh
+    sudo service hhvm restart
+
+    # This needs extra work to add to laravel nginx config or 192.168.33.10.xip.io apache config
+fi
+
+# Replace PHP with HHVM via symlinking
+if [ "$2" == "true" ]; then
+    sudo /usr/bin/update-alternatives --install /usr/bin/php php /usr/bin/hhvm 60
+fi
