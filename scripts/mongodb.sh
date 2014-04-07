7a1b20ed2fb969e92d74fb6edf885cbd88278d97
Hosmel Quintana <hosmel@otterpro.com>
2014-04-07 11:03:46 -0600

    add mongodb php extencion

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -11,3 +11,30 @@ sudo apt-get update
 
 # Install MongoDB
 sudo apt-get -y install mongodb-10gen
+
+# Test if PHP is installed
+php -v > /dev/null 2>&1
+PHP_IS_INSTALLED=$?
+
+if [[ $PHP_IS_INSTALLED -eq 0 ]]; then
+    # install dependencies
+    sudo apt-get -y install php-pear php5-dev
+
+    # install php extencion
+    sudo pecl install mongo
+
+    # add extencion file and restart service
+    echo 'extension=mongo.so' | sudo tee /etc/php5/mods-available/mongo.ini
+
+    if ps ax | grep -v grep | grep apache2 > /dev/null; then
+        ln -s /etc/php5/mods-available/mongo.ini /etc/php5/apache2/conf.d/mongo.ini
+        # echo 'extension=mongo.so' | sudo tee /etc/php5/mods-available/mongo.ini
+        sudo service apache2 restart
+    fi
+
+    if ps ax | grep -v grep | grep php-fpm > /dev/null; then
+        ln -s /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/mongo.ini
+        # echo 'extension=mongo.so' | sudo tee /etc/php5/fpm/conf.d/mongo.ini
+        sudo service php5-fpm restart
+    fi
+fi
