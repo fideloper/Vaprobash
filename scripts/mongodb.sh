c1abe57f52f89d904f3b198d92b71607598b77aa
Juan Manuel Torres <kinojman@gmail.com>
2014-04-29 19:14:51 -0700

    [Fixes ##288] Get the version of PHP and if it matches version 5.3.10 (which is the one distributed by ubuntu 12.04 by default) It will create the mongo.ini in the correct location.

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -25,10 +25,26 @@ if [ $PHP_IS_INSTALLED -eq 0 ]; then
     sudo pecl install mongo < answers.txt
     rm answers.txt
 
-    # add extencion file and restart service
-    echo 'extension=mongo.so' | sudo tee /etc/php5/mods-available/mongo.ini
+    # Find php version
+    regex="PHP [0-9][.][0-9][.][0-9][0-9]"
+    incoming_string=$(php -v | grep 'PHP ' -m 1)
 
-    ln -s /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/mongo.ini
-    ln -s /etc/php5/mods-available/mongo.ini /etc/php5/cli/conf.d/mongo.ini
+    if [[ $incoming_string =~ $regex ]]; then
+        php_version=${BASH_REMATCH[0]}
+    fi
+
+    # Add extension file and restart service
+    if [[ $php_version == "PHP 5.3.10" ]]; then
+        echo "matched " $php_version
+        echo 'extension=mongo.so' | sudo tee /etc/php5/conf.d/mongo.ini
+    else
+        echo "matched " $php_version
+        echo 'extension=mongo.so' | sudo tee /etc/php5/mods-available/mongo.ini
+
+        ln -s /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/mongo.ini
+        ln -s /etc/php5/mods-available/mongo.ini /etc/php5/cli/conf.d/mongo.ini
+    fi
+
     sudo service php5-fpm restart
+
 fi
