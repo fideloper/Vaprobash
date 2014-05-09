15c0f0e2a888e4d01247d51e320da71109300123
fideloper <fideloper@gmail.com>
2014-05-09 08:01:45 -0400

    Fix #305 and #306 - FPM socket needs to be readable by web servers

--- scripts/php.sh
+++ scripts/php.sh
@@ -21,6 +21,12 @@ sudo apt-get update
 # Install PHP
 sudo apt-get install -y php5-cli php5-fpm php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-gmp php5-mcrypt php5-xdebug php5-memcached php5-imagick php5-intl
 
+# Allow php5-fpm (socket) to listen to www-data:www-data
+sudo sed -i "s/;listen.owner/listen.owner/" /etc/php5/fpm/pool.d/www.conf
+sudo sed -i "s/;listen.group/listen.group/" /etc/php5/fpm/pool.d/www.conf
+sudo sed -i "s/;listen.mode/listen.mode/"   /etc/php5/fpm/pool.d/www.conf
+
+
 # xdebug Config
 cat > $(find /etc/php5 -name xdebug.ini) << EOF
 zend_extension=$(find /usr/lib/php5 -name xdebug.so)
