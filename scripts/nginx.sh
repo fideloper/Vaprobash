4355cafd2637b03a59fe961519b6ac40b9e01a7c
fideloper <fideloper@gmail.com>
2014-07-12 13:38:16 -0400

    moved php group change to php installer assigned www-data group to user vagrant

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -52,12 +52,8 @@ sed -i 's/sendfile on;/sendfile off;/' /etc/nginx/nginx.conf
 sed -i "s/user www-data;/user vagrant;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
 
-sed -i "s/user = www-data/user = vagrant/" /etc/php5/fpm/pool.d/www.conf
-sed -i "s/group = www-data/group = vagrant/" /etc/php5/fpm/pool.d/www.conf
-
-sed -i "s/;listen\.owner.*/listen.owner = vagrant/" /etc/php5/fpm/pool.d/www.conf
-sed -i "s/;listen\.group.*/listen.group = vagrant/" /etc/php5/fpm/pool.d/www.conf
-sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php5/fpm/pool.d/www.conf
+# Add vagrant user to www-data group
+usermod -a -G www-data vagrant
 
 # Nginx enabling and disabling virtual hosts
 curl --silent -L $github_url/helpers/ngxen.sh > ngxen
--- scripts/php.sh
+++ scripts/php.sh
@@ -40,6 +40,15 @@ else
     # Set PHP FPM allowed clients IP address
     sudo sed -i "s/;listen.allowed_clients/listen.allowed_clients/" /etc/php5/fpm/pool.d/www.conf
 
+    # Set run-as user for PHP5-FPM processes to user/group "vagrant"
+    # to avoid permission errors from apps writing to files
+    sudo sed -i "s/user = www-data/user = vagrant/" /etc/php5/fpm/pool.d/www.conf
+    sudo sed -i "s/group = www-data/group = vagrant/" /etc/php5/fpm/pool.d/www.conf
+
+    sudo sed -i "s/;listen\.owner.*/listen.owner = vagrant/" /etc/php5/fpm/pool.d/www.conf
+    sudo sed -i "s/;listen\.group.*/listen.group = vagrant/" /etc/php5/fpm/pool.d/www.conf
+    sudo sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php5/fpm/pool.d/www.conf
+
 
     # xdebug Config
     cat > $(find /etc/php5 -name xdebug.ini) << EOF
