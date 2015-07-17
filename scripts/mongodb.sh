edb30ceb3fc761cce411588c3805af43dc736466
Robert <bruhnrp@gmail.com>
2015-07-17 11:04:08 -0400

    Added ability to choose MongoDB version.

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -4,7 +4,14 @@ echo ">>> Installing MongoDB"
 
 # Get key and add to sources
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+# Make MongoDB connectable from outside world without SSH tunnel
+if [ $2 == "2.6" ]; then
+  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+elif [ $2 == "3.0" ]; then
+    echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+fi
+
 
 # Update
 sudo apt-get update
