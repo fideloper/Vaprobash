3eb93fb89fb703d9000d1c7fa4f2261b5c79f063
Felix Imobersteg <felix@whatwedo.ch>
2015-11-08 15:02:01 +0100

    Download docker-compose silently

--- scripts/docker-compose.sh
+++ scripts/docker-compose.sh
@@ -3,5 +3,5 @@
 echo ">>> Installing docker-compose"
 
 #Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.3.3/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L -s https://github.com/docker/compose/releases/download/1.3.3/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
