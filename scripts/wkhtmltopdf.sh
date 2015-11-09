8eeece7768a00a0be0d9320fbc5087618d956a41
Felix Imobersteg <felix@whatwedo.ch>
2015-11-09 20:17:56 +0100

    Use latest wkhtmltopdf version

--- scripts/wkhtmltopdf.sh
+++ scripts/wkhtmltopdf.sh
@@ -3,7 +3,7 @@
 echo ">>> Installing wkhtmltopdf"
 
 # Installwkhtmltopdf
-apt-get install -qq wkhtmltopdf xvfb
-echo 'xvfb-run --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf $*' > /usr/bin/wkhtmltopdf.sh
-chmod a+rx /usr/bin/wkhtmltopdf.sh
-ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
+apt-get install -qq wkhtmltopdf xvfb xfonts-75dpi
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+rm wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
