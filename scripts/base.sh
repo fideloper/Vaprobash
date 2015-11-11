3747871372606dc03bd1085318766249d2ddda13
Felix Imobersteg <felix@whatwedo.ch>
2015-11-11 10:50:56 +0100

    fix(language): a lot of the tools fails with the C.UTF-8 language setting

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,8 +3,9 @@
 echo "Setting Timezone & Locale to $3 & C.UTF-8"
 
 sudo ln -sf /usr/share/zoneinfo/$3 /etc/localtime
-sudo locale-gen C.UTF-8
-export LANG=C.UTF-8
+sudo apt-get install -qq language-pack-en
+sudo locale-gen en_US
+sudo update-locale LANG=en_US.UTF-8 LC_CTYPE=en_US.UTF-8
 
 echo "export LANG=C.UTF-8" >> /home/vagrant/.bashrc
 
