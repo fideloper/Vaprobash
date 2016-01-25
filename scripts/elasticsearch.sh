1a46d8084e89b51cfee23d5d1e391bccbb94ed92
Ueli Banholzer <ueli@whatwedo.ch>
2016-01-25 12:46:49 +0100

    feat(elasticsearch): let the user choose elasticsearch version

--- scripts/elasticsearch.sh
+++ scripts/elasticsearch.sh
@@ -3,7 +3,7 @@
 echo ">>> Installing Elasticsearch"
 
 # Set some variables
-ELASTICSEARCH_VERSION=2.1.1 # Check https://www.elastic.co/downloads/elasticsearch for latest version
+ELASTICSEARCH_VERSION=$1 # Check https://www.elastic.co/downloads/elasticsearch for latest version
 
 # Install prerequisite: Java
 # -qq implies -y --force-yes
