e6d995225da4e6bf0f30688dfa678ff52b6f7bc0
Andrey Zavarin <azavarin@gmail.com>
2016-06-02 12:12:13 +0300

    Update rabbitmq.sh
    Fix signing key path for RabbitMQ script
    Add -y for apt-get install rabbitmq-server for silent installation

--- scripts/rabbitmq.sh
+++ scripts/rabbitmq.sh
@@ -3,11 +3,11 @@
 echo ">>> Installing RabbitMQ"
 
 apt-get -y install erlang-nox
-wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-apt-key add rabbitmq-signing-key-public.asc
+wget https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
+apt-key add rabbitmq-release-signing-key.asc
 echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
 apt-get update
-apt-get install rabbitmq-server
+apt-get -y install rabbitmq-server
 
 rabbitmqctl add_user $1 $2
 rabbitmqctl set_permissions -p / $1 ".*" ".*" ".*"
