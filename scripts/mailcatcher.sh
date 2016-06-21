ec7c090cafa84859a45d867bad6ca845bd045dc4
Chris Pelzer <chrispelzer@gmail.com>
2016-06-21 03:30:57 -0400

    Install mailcatcher dependencies and then install mailcatcher using --ignore-dependencies to override mail 2.6.4 requiring ruby 2.0.0+

--- scripts/mailcatcher.sh
+++ scripts/mailcatcher.sh
@@ -26,13 +26,23 @@ if $(which rvm) -v > /dev/null 2>&1; then
 else
 	# Installing ruby dependency
 	# -qq implies -y --force-yes
-	sudo apt-get install -qq ruby-2.0.0-dev
+	sudo apt-get install -qq ruby1.9.1-dev
 
 	# Gem check
 	if ! gem -v > /dev/null 2>&1; then sudo aptitude install -y libgemplugin-ruby; fi
 
+	# Install Mailcatcher gem dependencies, otherwise Ruby 2.0.0+ is required
+	gem install --no-rdoc --no-ri mail -v 2.6.3 # Last known working with Ruby < 2.0.0
+	gem install --no-rdoc --no-ri activesupport -v "~> 4.0"
+	gem install --no-rdoc --no-ri eventmachine -v 1.0.9.1
+	gem install --no-rdoc --no-ri rack -v "~> 1.5"
+	gem install --no-rdoc --no-ri sinatra -v "~> 1.2"
+	gem install --no-rdoc --no-ri skinny -v "~> 0.2.3"
+	gem install --no-rdoc --no-ri sqlite3 -v "~> 1.3"
+	gem install --no-rdoc --no-ri thin -v "~> 1.5.0"
+
 	# Install
-	gem install --no-rdoc --no-ri mailcatcher
+	gem install --no-rdoc --no-ri --ignore-dependencies mailcatcher -v "~> 0.6"
 fi
 
 # Make it start on boot
