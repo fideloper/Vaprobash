bc60e8c35ab2c49637947e32cb2bdcf425b72e71
Markus Bachmann <info@bachi.biz>
2016-10-26 18:47:10 +0200

    Disable interaction for rabbitmq

--- scripts/rabbitmq.sh
+++ scripts/rabbitmq.sh
@@ -7,7 +7,7 @@ wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
 apt-key add rabbitmq-signing-key-public.asc
 echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
 apt-get update
-apt-get install rabbitmq-server
+apt-get install -y rabbitmq-server
 
 rabbitmqctl add_user $1 $2
 rabbitmqctl set_permissions -p / $1 ".*" ".*" ".*"
