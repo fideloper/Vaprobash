de8c32b19f6b89d7d5e5cdec081d0f2739c3842d
Dan Storm <storm@err0r.dk>
2017-05-11 09:06:51 +0200

    Updated Docker to use Docker CE

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,16 +2,20 @@
 
 echo ">>> Installing Docker"
 
+# Install required packaes (beyond base packages)
+sudo apt-get install -qq apt-transport-https ca-certificates
+
 # Add Key
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add Repository
-sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
 sudo apt-get update
 
 # Install Docker
 # -qq implies -y --force-yes
-sudo apt-get install -qq lxc-docker
+sudo apt-get install -qq docker-ce
 
 # Make the vagrant user able to interact with docker without sudo
 if [ ! -z "$1" ]; then
