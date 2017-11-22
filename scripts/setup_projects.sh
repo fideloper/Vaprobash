b45569d835c882cb0e48cddf57826979862880e3
Rob Attfield <emailme@robertattfield.com>
2017-11-22 23:48:09 +1300

    Troubleshooting custom projects in 'scripts/projects'.

--- scripts/setup_projects.sh
+++ scripts/setup_projects.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-chmod -R u+x projects/
+chmod -R u+x ./projects
 
-for file in projects/*.sh; do
+for file in ./projects/*.sh; do
     [ -f "$file" ] && [ -x "$file" ] && "$file";
 done
