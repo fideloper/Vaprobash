5a78a339b2cae4d119d6256241970db6094c3cc0
Rob Attfield <emailme@robertattfield.com>
2019-01-12 13:59:53 +1300

    Updated scripts.

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -17,8 +17,9 @@ BASHRC=~/.bashrc
 if [[ $NODE_IS_INSTALLED -ne 0 ]]; then
 
     echo ">>> Installing latest stable Node LTS - 6.X"
-
-	sudo apt-get update
+	
+    sudo apt-get -f -y upgrade;
+    sudo apt-get -f -y dist-upgrade;
 	sudo apt-get -y install python-software-properties
 	
     # Install NVM
