c0ac97e4e896e15cf2ce9785b45e9916608b1d47
Rob Attfield <emailme@robertattfield.com>
2021-01-26 16:57:29 +1300

    Minor improvements.

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -17,46 +17,46 @@ BASHRC=~/.bashrc
 if [[ $NODE_IS_INSTALLED -ne 0 ]]; then
 
     echo ">>> Installing latest stable Node LTS - 10.X"
-	
+    
     sudo apt-get -f -y upgrade;
     sudo apt-get -f -y dist-upgrade;
-	sudo apt-get -y install python-software-properties
-	
+    sudo apt-get -y install python-software-properties
+    
     # Install NVM
-	curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-	sudo apt-get update
-	sudo apt-get -y install nodejs npm node-gyp
-	
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get update
+    sudo apt-get -y install nodejs npm node-gyp
+    
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+    # Install (optional) Global Node Packages
+
+    echo ">>> Start installing Global Node Packages"
+
+    sudo npm install -g grunt-cli --force; 
+    sudo npm install -g gulp --force; 
+    sudo npm install -g bower --force; 
+    sudo npm install -g yarn --force; 
+    sudo npm install -g gulp-concat-css --force; 
+    sudo npm install -g gulp-minify-css --force; 
+    sudo npm install -g gulp-clean-css --force; 
+    sudo npm install -g gulp-rename --force; 
+    sudo npm install -g gulp-ruby-sass --force; 
+    sudo npm install -g gulp-sourcemaps --force; 
+    sudo npm install -g gulp-uglify --force; 
+    sudo npm install -g notify-send --force; 
+    sudo npm install -g sw-precache-webpack-plugin --force; 
+    sudo npm install -g cross-env --force; 
+    sudo npm install -g laravel-mix --force; 
+    sudo npm install -g laravel-elixir --force;
 
     # Re-source user profiles
     # if they exist
     source $PROFILE
-	
-	# Re-source .bashrc if exists
+    
+    # Re-source .bashrc if exists
     source $BASHRC
 
 fi
-
-# Install (optional) Global Node Packages
-
-echo ">>> Start installing Global Node Packages"
-
-sudo npm install -g grunt-cli --force; 
-sudo npm install -g gulp --force; 
-sudo npm install -g bower --force; 
-sudo npm install -g yarn --force; 
-sudo npm install -g gulp-concat-css --force; 
-sudo npm install -g gulp-minify-css --force; 
-sudo npm install -g gulp-clean-css --force; 
-sudo npm install -g gulp-rename --force; 
-sudo npm install -g gulp-ruby-sass --force; 
-sudo npm install -g gulp-sourcemaps --force; 
-sudo npm install -g gulp-uglify --force; 
-sudo npm install -g notify-send --force; 
-sudo npm install -g sw-precache-webpack-plugin --force; 
-sudo npm install -g cross-env --force; 
-sudo npm install -g laravel-mix --force; 
-sudo npm install -g laravel-elixir --force;
 
 sudo apt -f -y autoremove --purge
